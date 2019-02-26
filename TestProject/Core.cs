e0bbf2058f4bef59625af45039ca3cf92b4aa8eb
UserR00T <contact@UserR00T.com>
2019-02-26 14:18:23 +0100

    Removed HasReturnValue as you can use out as parameter (didn't know that, wow)

--- TestProject/Core.cs
+++ TestProject/Core.cs
@@ -11,7 +11,6 @@ namespace TestProject
 	public class Core
 	{
 		[HookAttribute.Hook("Injected.Method3")]
-		[HasReturnValue]
 		public static bool InjectedMethod_M(Injected instance)
 		{
 			return true;
--- UniversalUnityHooks/Attributes/HookAttributes.cs
+++ UniversalUnityHooks/Attributes/HookAttributes.cs
@@ -27,45 +27,13 @@ namespace UniversalUnityHooks.Attributes
 			if (!attributes.ContainsKey(nameof(HookAttributes)))
 				return 0;
 			var injectedCorrectly = 0;
-			var returnValueAttributes = new Dictionary<MethodDefinition, AttributeData>();
-			returnValueAttributes = attributes[nameof(HasReturnValueAttribute)].ToDictionary(x => x.Method, y => y);
 			foreach (var hook in attributes[nameof(HookAttributes)])
 			{
 				hook.TargetData = Cecil.ConvertStringToClassAndMethod(hook.Attribute.ConstructorArguments[0].Value.ToString(), assemblyDefinition);
 				if (hook.TargetData == null)
 					continue;
-				if (!returnValueAttributes.ContainsKey(hook.Method))
-				{
-					if (Cecil.Inject(hook.TargetData, hook))
-						++injectedCorrectly;
-					continue;
-				}
-				ConsoleHelper.WriteMessage($"F1: {hook.Method.FullName}: {hook.Method.ReturnType.Name} | F2: {hook.TargetData.methodDefinition.FullName}: {hook.TargetData.methodDefinition.ReturnType.Name}");
-				if (hook.Method.ReturnType.Name != hook.TargetData.methodDefinition.ReturnType.Name)
-				{
-					ConsoleHelper.WriteError($"Return types of {hook.Method.Name} and {hook.TargetData.methodDefinition.Name} do not match. They need to match to add the [HasReturnValue] attribute.");
-					continue;
-				}
-				ConsoleHelper.WriteMessage($"Mdef: {hook.TargetData.methodDefinition} | Tdef: {hook.TargetData.typeDefinition}");
-				var il = hook.TargetData.methodDefinition.Body.GetILProcessor();
-				var firstInstruction = il.Body.Instructions.First();
-				var method = hook.TargetData.methodDefinition;
-				Instruction lastInstruction = null;
-				Instruction currInstruction = null;
-				if (!method.IsStatic && !method.DeclaringType.IsSealed)
-				{
-					currInstruction = il.Create(OpCodes.Ldarg_0);
-					if (firstInstruction != null)
-						il.InsertBefore(firstInstruction, currInstruction);
-					else
-						il.Append(currInstruction);
-				}
-				lastInstruction = currInstruction;
-				currInstruction = il.Create(OpCodes.Call, assemblyDefinition.MainModule.Import(hook.Method));
-				il.InsertAfter(lastInstruction ?? firstInstruction, currInstruction);
-				lastInstruction = currInstruction;
-				il.InsertAfter(lastInstruction, Instruction.Create(OpCodes.Ret));
-				injectedCorrectly += 2; // HookAttribute and HasReturnValue both succeeded, add two
+				if (Cecil.Inject(hook.TargetData, hook))
+					++injectedCorrectly;
 			}
 			return injectedCorrectly;
 		}
