2c52c3ae9308d7b3bf3c246427f5dc7e52c156a3
UserR00T <contact@UserR00T.com>
2020-09-17 15:10:28 +0200

    Added constructors to Logger<T>

--- UniversalUnityHooks.Logging/Logger[T].cs
+++ UniversalUnityHooks.Logging/Logger[T].cs
@@ -5,5 +5,14 @@ namespace UniversalUnityHooks.Logging
     public class Logger<T> : Logger, ILogger<T>
     {
         public override string Id { get; } = typeof(T).FullName;
+
+        public Logger() : base()
+        {
+        }
+
+        public Logger(LoggerSettings settings) : base(settings)
+        {
+        }
+        
     }
 }
